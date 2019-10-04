8d5e2ec7424a5eeefb0727316d1ad2f8f6426999
lilianyc <lilianyangcrosson@gmail.com>
2019-10-04 16:19:41 +0200

    Made data-finding relative

--- src/deepdrug.py
+++ src/deepdrug.py
@@ -4,17 +4,34 @@
 Created on Mon Sep 30 15:21:17 2019
 
 @author: lyang_crosson
+
+
+Visualize 3*3*3*14 voxel ?
+What about change in 3d orientation of voxel ? should not because of CNN
+
+different batch sizes of 16, 32, 64, and 128 yield a comparable
+performance, and 50 epochs are sufficient to reach the convergence. A 5-fold cross-validation
+
+see CAM for significant binding regions ?
+
+TOGH C1 dataset
+
+# Pockets bind
+nucleotide, heme and steroid
+
+took representative sites with TC
 """
 
 from pathlib import Path
 import numpy as np
 
+data_dir = Path(__name__).resolve().parent.parent.joinpath("data/")
 NB_PROT = 10
 
-# MAke sure in right dir
 # numpy loading directly is horrendously slow.
-prot_list = [file for file in Path(".").glob("../data/deepdrug3d_voxel_data/*.npy")
-            if not file.name.startswith("._")]
+prot_list = [file for file in data_dir.glob("deepdrug3d_voxel_data/*.npy")
+             if not file.name.startswith("._")]
+
 # list(Path(".").glob("../data/deepdrug3d_voxel_data/[!._]*.npy"))
 x_train = [np.load(file) for file in prot_list[:NB_PROT]]
 
