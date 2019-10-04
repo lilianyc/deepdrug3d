44ca836bd23b11b9932731d880316026eecc6091
lilianyc <lilianyangcrosson@gmail.com>
2019-10-04 17:14:12 +0200

    Retrieve information on pocket type

--- src/deepdrug.py
+++ src/deepdrug.py
@@ -14,7 +14,7 @@ performance, and 50 epochs are sufficient to reach the convergence. A 5-fold cro
 
 see CAM for significant binding regions ?
 
-TOGH C1 dataset
+TOUGH C1 dataset
 
 # Pockets bind
 nucleotide, heme and steroid
@@ -28,22 +28,39 @@ import numpy as np
 data_dir = Path(__name__).resolve().parent.parent.joinpath("data/")
 NB_PROT = 10
 
+# Raise error if deepdrug directory not found.
+assert data_dir.joinpath("deepdrug3d_voxel_data/").is_dir(), \
+       "No directory deepdrug3d_voxel_data/ found in data/"
+
 # numpy loading directly is horrendously slow.
 prot_list = [file for file in data_dir.glob("deepdrug3d_voxel_data/*.npy")
              if not file.name.startswith("._")]
+
+# Assign class filenames to lists
+with open(data_dir.joinpath("control.list"), "r") as file1, \
+     open(data_dir.joinpath("heme.list"), "r") as file2, \
+     open(data_dir.joinpath("nucleotide.list"), "r") as file3, \
+     open(data_dir.joinpath("steroid.list"), "r") as file4:
+
+    control_list = [filename.strip() for filename in file1.readlines()]
+    heme_list = [filename.strip() for filename in file2.readlines()]
+    nucleotide_list = [filename.strip() for filename in file3.readlines()]
+    steroid_list = [filename.strip() for filename in file4.readlines()]
+
+# TODO: Combine the class information from the files to the list.
 
 # list(Path(".").glob("../data/deepdrug3d_voxel_data/[!._]*.npy"))
 x_train = [np.load(file) for file in prot_list[:NB_PROT]]
 
 np.unique(x_train[0])
 
-# See how data changed
-# d = c.reshape(32,32,32,14)
-# np.squeeze
-
 # =============================================================================
 # See shape
 # =============================================================================
+
+# See how data changed
+# d = c.reshape(32,32,32,14)
+# np.squeeze
 
 tmp = np.load(prot_list[0])
 tmp_shp = tmp.reshape(14,32,32,32)
